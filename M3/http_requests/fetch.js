0c3c4a62b1d6a04a30d8fd87a7de5949e313d41f
jasurhakimov007@gmail.com <jasurhakimov007@gmail.com>
2023-07-12 19:12:19 +0200

    fetch http request example

--- M3/http_requests/fetch.js
+++ M3/http_requests/fetch.js
@@ -1,17 +1,45 @@
 const getBtn = document.getElementById('get-btn');
 const postBtn = document.getElementById('post-btn');
 
-const getData = () => {
-    fetch('https://reqres.in/api/users')
+const sendHttpRequest = (method, url, data)=>{
+    return fetch(url, {
+        method: method,
+        body: JSON.stringify(data),
+        headers: data ?{'content-type': 'application/json'} : {}
+    })
     .then( response=>{
+        if(response.status >=400){
+            // !response.ok
+            return response.json().then(errResData =>{
+                const error = new Error('Oops somthing went wrong!');
+                error.data = errResData ;
+                throw error;
+            })
+        }
         return response.json();
-    })
-    .then( responseData =>{
-        console.log(responseData);
-    })
+    });
 };
 
-const sendData = () => {};
+const getData = () => {
+    sendHttpRequest('GET', 'https://reqres.in/api/users')
+    .then( responseData =>{
+        console.log(responseData);
+    });
+};
+
+const sendData = () => {
+    sendHttpRequest('POST', 'https://reqres.in/api/register', {
+        email: "eve.holt@reqres.in", 
+        // password: "pistol" //if email or password is wrong it will be rejected and catch block console logs
+    
+    })
+    .then(responseData=>{
+        console.log(responseData);
+    })
+    .catch(err=>{
+        console.log(err);
+    });
+};
 
 getBtn.addEventListener('click', getData);
 postBtn.addEventListener('click', sendData);
