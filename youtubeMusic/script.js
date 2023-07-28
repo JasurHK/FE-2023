539b6c4e9c4f032dc34d60412e74f9db5420075d
jasurhakimov007@gmail.com <jasurhakimov007@gmail.com>
2023-07-28 10:45:46 +0200

    toggle function added to user accaunt botton

---
+++ youtubeMusic/script.js
@@ -0,0 +1,9 @@
+function toggleElement() {
+    var element = document.getElementById("accaunt");
+    if (element.style.display === "none") {
+      element.style.display = "block"; // Or any other display value you want to use
+    } else {
+      element.style.display = "none";
+    }
+  }
+  
