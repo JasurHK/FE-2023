23b7eec9f449c2c9de8d753aebc3379871d08a64
jasurhakimov007@gmail.com <jasurhakimov007@gmail.com>
2023-07-31 23:16:41 +0200

    to be continued

--- youtubeMusic/script.js
+++ youtubeMusic/script.js
@@ -1,9 +1,11 @@
 function toggleElement() {
-    var element = document.getElementById("accaunt");
+    let element = document.getElementById("accaunt");
     if (element.style.display === "none") {
       element.style.display = "block"; // Or any other display value you want to use
     } else {
       element.style.display = "none";
     }
-  }
+}
+
+
   
