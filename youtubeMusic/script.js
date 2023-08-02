aa7dc312a3c68a8b7c26e7f4e69d49f1ce826736
jasurhakimov007@gmail.com <jasurhakimov007@gmail.com>
2023-08-02 16:14:38 +0200

    .....

--- youtubeMusic/script.js
+++ youtubeMusic/script.js
@@ -6,6 +6,105 @@ function toggleElement() {
       element.style.display = "none";
     }
 }
+// this adds home section to the main when page is succesfully loades
+window.onload = function() {
+    const newSection = document.createElement('section');
+    newSection.id = 'home';
+    newSection.className='home';
+    newSection.innerHTML = '';
+    
+    document.main.appendChild(newSection);
+  };
 
 
+
+
+
+const textInput = document.getElementById('song-name');
+const output = document.getElementById('home');
+
+textInput.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    const inputValue = textInput.value;
+    output.innerHTML= `
+    <div class="home-list">
+        <ul>
+            <li>
+                <a href="#">Song</a>
+            </li>
+            <li>
+                <a href="#">Video</a>
+                </li>
+            <li>
+                <a href="#">Albums</a>
+            </li>
+            <li>
+                <a href="#">Artists</a>
+            </li>
+            <li>
+                <a href="#">Featured playlists</a>
+            </li>
+            <li>
+                <a href="#">Community playlists</a>
+            </li>
+            <li>
+                <a href="#">Profiles</a>
+            </li>
+        </ul>
+    </div>
+            <div style="margin:15px 50px;">
+                <h1>Top result</h1>
+            </div>
+            <div class="output">
+                <div class="output-left">
+                    <div class="output-left_img">
+                        <a href="#"><i class="fa fa-play" aria-hidden="true"></i><img src="https://picsum.photos/150" alt=""></a>
+                        <div class="output-left_cont">
+                            <h2>the night we met</h2>
+                            <p>song.<span></span>something</p>
+                            <div class="output-left_btn">
+                                <button class="play"><i class="fa fa-play" aria-hidden="true"></i>play</button>
+                                <button><i class="fa fa-plus-square" aria-hidden="true"></i>save</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="output-right">
+                    <h3>MORE FROME YOUTUBE</h3>
+                    <div class="output-right_list">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <img src="https://picsum.photos/70" alt="">
+                                    <div>
+                                        <p>song name-artist name</p>
+                                        <small>band name. <span>124kviews</span> .duration</small>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="https://picsum.photos/70" alt="">
+                                    <div>
+                                        <p>song name-artist name</p>
+                                        <small>band name. <span>124kviews</span> .duration</small>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="https://picsum.photos/70" alt="">
+                                    <div>
+                                        <p>song name-artist name</p>
+                                        <small>band name. <span>124kviews</span> .duration</small>
+                                    </div>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+    `;
+  }
+});
   
