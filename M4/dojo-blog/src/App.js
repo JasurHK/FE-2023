e8a24c55085514fb3ea0d651e08d9f3b0573f288
jasurhakimov007@gmail.com <jasurhakimov007@gmail.com>
2023-08-15 12:17:01 +0200

    multiple components added

--- M4/dojo-blog/src/App.js
+++ M4/dojo-blog/src/App.js
@@ -5,7 +5,7 @@ import Home from './Home';
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <div className="content">
         <Home />
       </div> 
