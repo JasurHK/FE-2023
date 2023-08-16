aba1f6e892be4f7f074d590513e07d035789cca2
jasurhakimov007@gmail.com <jasurhakimov007@gmail.com>
2023-08-16 16:16:23 +0200

    Link component inported and repleced with <a> tag

--- M4/dojo-blog/src/Navbar.js
+++ M4/dojo-blog/src/Navbar.js
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">Create</a>
+                <Link to="/">Home</Link>
+                <Link to="/Create">Create</Link>
             </div>
         </nav>
     );
