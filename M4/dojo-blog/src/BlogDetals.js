13519ed22ef1062debab73eb834ea3bb6a4924e1
jasurhakimov007@gmail.com <jasurhakimov007@gmail.com>
2023-08-18 09:53:50 +0200

    click evant added to delete button and it makes DELETE request to delete the blog

--- M4/dojo-blog/src/BlogDetals.js
+++ M4/dojo-blog/src/BlogDetals.js
@@ -1,9 +1,21 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const history = useHistory();
   const { data:blog, isPanding, error} = useFetch('http://localhost:8000/blogs/' + id);
+
+  const handleClick= ()=>{
+    fetch('http://localhost:8000/blogs/'+blog.id , {
+      method: 'DELETE'
+    })
+    .then(()=>{
+      history.push('/')
+  })
+  }
+
   return (
     <div className="blog-details">
       {isPanding && <div> Loading...</div>}
@@ -15,6 +27,7 @@ const BlogDetails = () => {
             <div>{blog.body}</div>
         </article>
       )}
+      <button onClick={handleClick}>delete</button>
     </div>
   );
 }
