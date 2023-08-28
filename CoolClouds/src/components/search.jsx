02d2e7aa43ed78dbe99a6a5db2b8fbe3237f4358
jasurhakimov007@gmail.com <jasurhakimov007@gmail.com>
2023-08-28 18:04:21 +0200

    serchBar

--- CoolClouds/src/components/search.jsx
+++ CoolClouds/src/components/search.jsx
@@ -1,25 +1,58 @@
 import { useState } from "react";
+import "../styles/searchBar.css"
 
 
 
 
-const Search = (onSearch) => {
+const Search = ({onSearch}) => {
     const [searchQuery, setSearchQuery] = useState('');
+    const [isSearchVisible, setSearchVisible] = useState(false);
 
-    const handleSubmit = () => {
+    const handleSubmit = async() => {
         if(searchQuery) {
-            onSearch(searchQuery)
+            await onSearch(searchQuery)
+            setSearchVisible(false); // Hide the search bar after submitting
+            setSearchQuery(''); // Clear the search query
         }
+    }
+    const handleKeyDown = async(event) => {
+        if (event.key === 'Enter') {
+          await handleSubmit();
+        //   let element = document.getElementById("searchBar");
+        //   let secondE = document.getElementById("searchBtn");
+        //   element.style.display = "none";
+        //   element.value = ""
+        //   secondE.style.display = "block";
+        } 
+      };
+    const toggleElement=()=> {
+        setSearchVisible(!isSearchVisible);
+        handleSubmit();
+        // let element = document.getElementById("searchBar");
+        // let secondE = document.getElementById("searchBtn");
+        // if (element.style.display === "none") {
+        //   element.style.display = "block"; // Or any other display value you want to use
+        //   secondE.style.display = "none"; 
+        // } else {
+        //   element.style.display = "none";
+        //   secondE.style.display = "block";
+        // }
     }
 
   return (
-    <div>
+    <div className={`searchBar ${isSearchVisible ? 'active' : ''}`}>
         <input
+            id="searchBar"
             type="text" 
+            value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
             placeholder="Search for a city..."
+            onKeyDown={handleKeyDown}
+            style={{ transform: isSearchVisible ? 'scaleX(1)' : 'scaleX(0)' }}
         />
-        <button onClick={handleSubmit}>Search</button>
+        <button onClick={toggleElement} id="searchBtn">
+            {isSearchVisible ? 'Enter' : 'Search'}
+        </button>
     </div>
   )
 }
