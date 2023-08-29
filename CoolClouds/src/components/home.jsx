49b0c0122e3a963e7825631733963526a33ff610
jasurhakimov007@gmail.com <jasurhakimov007@gmail.com>
2023-08-29 16:47:45 +0200

    weather card component

--- CoolClouds/src/components/home.jsx
+++ CoolClouds/src/components/home.jsx
@@ -1,9 +1,12 @@
-const HomeWeather = (weather) => {
+import "../styles/home.css"
+const HomeWeather = ({weather}) => {
     return (
         <>
-            <div className="weatherCard">
-                <h1>hello bitches</h1>
-            </div> 
+            <div className="home">
+                {weather&& <h1>Warsav</h1>}
+                {weather&& <p>{weather.vt1nowcast.narrative256Char}</p>}
+                
+            </div>
         </>
     );
 }
