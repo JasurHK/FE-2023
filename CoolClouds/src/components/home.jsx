39822676c3134a74aa26702fb4012ae48ea58cc3
jasurhakimov007@gmail.com <jasurhakimov007@gmail.com>
2023-09-05 00:05:06 +0200

    icon handling function created

--- CoolClouds/src/components/home.jsx
+++ CoolClouds/src/components/home.jsx
@@ -11,14 +11,40 @@ import snowyNight from '../assets/snowy-6.svg'
 import weatherIcon from '../assets/weather.svg'
 import sunset from '../assets/weather_sunset.svg'
 
+function handleIcons(icons, idToFind) {
+    const foundIcon = icons.find(icon => icon.id === idToFind);
+    return foundIcon || null; // Return null if no matching icon is found
+  }
+  
 
+const icons = [
+    {id:'01' , name :daySunny},
+    {id:'02' , name :nigthClear},
+    {id:'03' , name :cloudyDay},
+    {id:'04' , name :cloudyNight},
+    {id:'05' , name :rainyDay},
+    {id:'06' , name :rainyNight},
+    {id:'07' , name :thunder},
+    {id:'08' , name :snowyDay},
+    {id:'09' , name :snowyNight},
+    {id:'10' , name :weatherIcon},
+]
+
+const iconIdToFind = '10';
+const foundIcon = handleIcons(icons, iconIdToFind);
+
+if (foundIcon) {
+  console.log("Found Icon:", foundIcon.name);
+} else {
+  console.log("Icon not found.");
+}
 
 const HomeWeather = ({weather}) => {
    
     return (
         <div className="container">
             <div className="box">
-                <img src={thunder} alt="" />
+                <img src={foundIcon.name} alt="" />
                 {!weather[0] && 
                 <div className="loader">
                     <div data-glitch="Loading..." className="glitch">Loading...</div>
