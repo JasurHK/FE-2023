cbfe2af2faae043d212c15908f17acaf3c56e46a
jasurhakimov007@gmail.com <jasurhakimov007@gmail.com>
2023-09-06 10:13:18 +0200

    ...!

--- SpinCruise/src/App.jsx
+++ SpinCruise/src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import ReactDOM from 'react-dom'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="card">hello</div>
+      <div className="nav bg-light d-flex justify-content-between align-items-center">
+        <h1 className='text-info' >SpinCruise</h1>
+      </div>
     </>
   )
 }
