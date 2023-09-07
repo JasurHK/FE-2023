ab49be9f230eedd8f6f645c928a9b3c223b79b98
jasurhakimov007@gmail.com <jasurhakimov007@gmail.com>
2023-09-07 10:12:22 +0200

    ...!

--- SpinCruise/src/App.jsx
+++ SpinCruise/src/App.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import daySunny from './assets/banner.jpg'
 
 
 function App() {
@@ -9,6 +10,9 @@ function App() {
     <>
       <div className="nav bg-light d-flex justify-content-between align-items-center">
         <h1 className='text-info' >SpinCruise</h1>
+      </div>
+      <div className=" container container-fluid">
+        <img src={daySunny} className="img-fluid" alt="..."/> 
       </div>
     </>
   )
